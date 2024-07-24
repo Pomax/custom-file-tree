12e766a790a54b6426ca5ad0c78be82ac2462053
Pomax <pomax@nihongoresources.com>
2024-07-24 09:11:01 -0700

    Update index.js
    there is no API route on github.io

--- public/index.js
+++ public/index.js
@@ -27,3 +27,27 @@ const eventList = [
 eventList.forEach((type) =>
   fileTree.addEventListener(type, ({ detail }) => detail.grant())
 );
+
+const testContent = [
+  `dist/README.md`,
+  `dist/file-tree.esm.js`,
+  `dist/file-tree.esm.min.js`,
+  `dist/old/README.old`,
+  `dist/old/file-tree.esm.js`,
+  `dist/old/file-tree.esm.min.js`,
+  `public/index.html`,
+  `public/index.js`,
+  `public/README.md`,
+  `src/dir-entry.js`,
+  `src/file-entry.js`,
+  `src/file-tree.css`,
+  `src/file-tree.js`,
+  `src/README.md`,
+  `src/utils.js`,
+  `test/cake.because.why.not`,
+  `test/README.md`,
+  `package.json`,
+  `README.md`,
+];
+
+fileTree.setContent(testContent);
